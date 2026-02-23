a4aef6c28a0da9d29e140ee5db12099623e4ceb4
Ashen1919 <2303S0834@saegis.ac.lk>
2026-02-23 20:33:15 +0530

    work with matplotlib library

---
+++ Object_detection/multiple_points.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xPoints = np.array([0, 15, 20, 31])
+yPoints = np.array([5, 13, 17, 29])
+
+plt.plot(xPoints, yPoints)
+plt.show()
---
+++ Object_detection/plot_without_line.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xPoints = np.array([0, 15])
+yPoints = np.array([5, 13])
+
+plt.plot(xPoints, yPoints, 'o')
+plt.show()
